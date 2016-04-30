bd7153df407561a564ccaf7bf5c0927f2e44b820
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-04-30 16:09:38 +0100

    Update

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var Robinhood = require('robinhood');
 var CONFIG = require('./config.json');
 
-Robinhood({
-  username: CONFIG.username,
-  password: CONFIG.password
-}).orders(function handleResponse(error, response, body) {
-    if (error) {
+var robinhood = Robinhood({
+    username: CONFIG.username,
+    password: CONFIG.password
+  }, function handleAuthentication() {
+
+    robinhood.orders(function handleResponse(error, response, body) {
+      if (error) {
         console.error(error);
         process.exit(1);
-    }
+      }
 
-    console.log(body);
-});
+      console.log(body);
+    });
+
+  });
